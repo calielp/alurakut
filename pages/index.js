5c8388e0d46ef43edcecf676dd2422bc1acdbb84
Caliel Pires <75309414+calielp@users.noreply.github.com>
2021-07-17 15:33:27 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import nookies from 'nookies';
 import jwt from 'jsonwebtoken';
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
-import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/alurakutcommons'
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
 function ProfileSidebar(propriedades) {
